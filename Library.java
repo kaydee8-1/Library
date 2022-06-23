fd0dfdfa14a6832596c3554e306c5fe279ee602b
kaydee8-1 <87949390+kaydee8-1@users.noreply.github.com>
2022-06-23 22:14:25 +0100

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,22 @@
+import java.sql.SQLOutput;
+
+class Library {
+    public static void main(String[] args) {
+        final String name = "Library v0.1";
+
+        String title = "W Pustyni i Puszczy";
+        String author = "Henryk Sienkiewicz";
+        int releaseDate = 2010;
+        int pages = 296;
+        String publisher = "Greg";
+        String isbn = "1234567890";
+
+        System.out.println(name);
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(releaseDate);
+        System.out.println(pages);
+        System.out.println(publisher);
+        System.out.println(isbn);
+    }
+}
